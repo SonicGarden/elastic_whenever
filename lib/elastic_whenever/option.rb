d4e23b7a1f0cf089d425e1be7d9d53b69e5d51b6
Morikiyo Inoha <morikiyo@sonicgarden.jp>
2023-01-13 15:14:01 +0900

    fix: remove cloudwatch_events_client

--- lib/elastic_whenever/option.rb
+++ lib/elastic_whenever/option.rb
@@ -26,7 +26,6 @@ module ElasticWhenever
     attr_reader :aws_config
     attr_reader :ecs_client
     attr_reader :iam_client
-    attr_reader :cloudwatch_events_client # FIXME: scheduler_client に置き換えて削除する
     attr_reader :scheduler_client
 
     class InvalidOptionException < StandardError; end
@@ -106,8 +105,8 @@ module ElasticWhenever
         opts.on('--iam-role name', 'IAM role name used by CloudWatch Events. Default: ecsEventsRole') do |role|
           @iam_role = role
         end
-        opts.on('--rule-state state', 'The state of the CloudWatch Events Rule. Default: ENABLED') do |state|
-          @rule_state = state
+        opts.on('--rule-state state', 'The state of the EventBridge Scheduler. Default: ENABLED') do |state|
+          @rule_state = state # FIXME: schedule_state にするべきか？
         end
         opts.on('--profile profile_name', 'AWS shared profile name') do |profile|
           @profile = profile
@@ -146,11 +145,6 @@ module ElasticWhenever
 
     def iam_client
       @iam_client ||= Aws::IAM::Client.new(aws_config)
-    end
-
-    # FIXME: scheduler_client に置き換えて削除する
-    def cloudwatch_events_client
-      @cloudwatch_events_client ||= Aws::CloudWatchEvents::Client.new(aws_config)
     end
 
     def scheduler_client
